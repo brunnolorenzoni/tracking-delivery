4d74f49043da99e27734b69b3b1e353f5f707a9c
Brunno Lorenzoni <brunno.lorenzoni@unicred.com.br>
2022-08-17 09:48:52 -0300

    reading multiples files

--- trajectories/trajectories-producer.ts
+++ trajectories/trajectories-producer.ts
@@ -3,33 +3,23 @@ import Producer from '../src/infrastructure/kafka/Producer'
 import { parse } from 'csv'
 import fs from 'fs'
 
-
-
 const { kafka } = new Kafka({ clientId: 'my-app-producer', brokers: ['localhost:9092'] })
 const producer = new Producer(kafka)
 
-const ids:Array<string> = []
 fs.readdirSync('./trajectories/data/').forEach(file => {
-  ids.push(file.split('.')[0]);
-});
-
-
-(async () => {
-
-  const sleep = (ms:number) => new Promise((r) => setTimeout(r, ms));
-
-  for await (const id of ids) {
+  (async () => {
+    const sleep = (ms:number) => new Promise((r) => setTimeout(r, ms));
     const parser = fs
-    .createReadStream(`./trajectories/data/${id}.csv`)
+    .createReadStream(`./trajectories/data/${file.split('.')[0]}.csv`)
     .pipe(parse({
-      columns: ['lat', 'lng', 'field', 'altitude', 'numberOfDays', 'date', 'time'],
+      columns: ['lat', 'lng', 'userId', 'altitude', 'numberOfDays', 'date', 'time'],
       delimiter: ',',
       trim: true,
     }));
     for await (const record of parser) {
-      const { lat, lng } = record
+      const { lat, lng, userId } = record
       const message = { 
-        key: `${id}`, 
+        key: `${userId}`, 
         value: JSON.stringify({ lat, lng })
       }
       await producer.sendMessage('topic',message)
@@ -37,6 +27,5 @@ fs.readdirSync('./trajectories/data/').forEach(file => {
       await producer.shutdown()
       await sleep(1000);
     }
-  }
-  process.exit(0)
-})()
+  })()
+});
